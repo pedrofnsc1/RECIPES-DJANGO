7af4d6fb4f945c4b6d6e161865913d159be8dda0
pedrofnsc1 <pedro.ricardo575@gmail.com>
2022-05-05 00:06:49 -0300

    Category, Recipe migrations first migration

--- recipes/models.py
+++ recipes/models.py
@@ -1,28 +1,26 @@
-
+from django.contrib.auth.models import User
 from django.db import models
-import uuid
 
 # Create your models here.
 
-class Category(models.model):
-    name = models.CharField(max_length=65),
+class Category(models.Model):
+    name = models.CharField(max_length=65)
 
 
-class Recipe(models.model):
-    title = models.CharField(max_length=65),
-    description = models.CharField(max_length=150),
-    #slug = models.SlugField(),
-    id = models.UUIDField(default=uuid.uuid4),
-    preparation_time = models.IntegerField(),
-    preparation_time_unit = models.CharField(help_text="Quantos Minutos levam para fazer o prato"),
-    servings = models.IntegerField(),
-    servings_unit = models.CharField(max_length=20),
-    preparation_steps = models.TextField(),
-    preparation_steps_is_html = models.BooleanField(default=True),
-    created_at = models.DateTimeField(auto_now_add=True),
-    updated_at = models.DateTimeField(auto_now=True),
-    is_published = models.BooleanField(default=False),
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/'),
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=150)
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=20)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
 
-    #My Foreign key#
+    # My Foreign key #
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
