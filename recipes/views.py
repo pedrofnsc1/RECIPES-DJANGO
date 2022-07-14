ee172c61bdafc5822da343477d49c5c5ea979657
pedronfnsc1 <pedro.ricardo575@gmail.com>
2022-07-13 23:11:01 -0300

    recipe-view - mudando visualização

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import get_list_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 # from utils.recipes.factory import make_recipe
 
 from recipes.models import Recipe
@@ -37,10 +37,12 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
-        pk=id, 
-        is_published=True
-    ).order_by('-id').first()
+    # recipe = Recipe.objects.filter(
+    #     pk=id, 
+    #     is_published=True
+    # ).order_by('-id').first()
+
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
 
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
